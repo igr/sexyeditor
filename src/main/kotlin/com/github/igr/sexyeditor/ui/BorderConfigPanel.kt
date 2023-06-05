ed4d18b4d8e2bf92630cb3f81eb207c2fff4e589
Igor Spasić <igor.spasic@gmail.com>
2023-06-05 23:34:14 +0200

    Regroup code

--- src/main/kotlin/com/github/igr/sexyeditor/ui/BorderConfigPanel.kt
+++ src/main/kotlin/com/github/igr/sexyeditor/ui/BorderConfigPanel.kt
@@ -14,33 +14,31 @@ class BorderConfigPanel : EditorConfigUI() {
         this.config = config
 
         nameTextField.text = config.name
-        matchTextField!!.text = config.editorGroup
+        matchTextField.text = config.editorGroup
         opacitySlider.value = (config.opacity * 100).toInt()
 
         positionComboBox.selectedIndex = config.position
-        positionOffsetTextField!!.text = config.positionOffset.toString()
+        positionOffsetTextField.text = config.positionOffset.toString()
         if (shrinkCheckBox.isSelected != config.shrink) {
             shrinkCheckBox.doClick()
         }
         shrinkSlider.value = config.shrinkValue
-        if (fixedPositionCheckBox!!.isSelected != config.fixedPosition) {
-            fixedPositionCheckBox!!.doClick()
+        if (fixedPositionCheckBox.isSelected != config.fixedPosition) {
+            fixedPositionCheckBox.doClick()
         }
-        if (shrinkToFitCheckBox!!.isSelected != config.shrinkToFit) {
-            shrinkToFitCheckBox!!.doClick()
+        if (shrinkToFitCheckBox.isSelected != config.shrinkToFit) {
+            shrinkToFitCheckBox.doClick()
         }
         if (randomCheckBox.isSelected != config.random) {
             randomCheckBox.doClick()
         }
-        if (slideshowCheckBox!!.isSelected != config.slideshow) {
-            slideshowCheckBox!!.doClick()
+        if (slideshowCheckBox.isSelected != config.slideshow) {
+            slideshowCheckBox.doClick()
         }
-        slideShowPause!!.text = config.slideshowPause.toString()
-        fileListModel!!.clear()
-        if (config.fileNames != null) {
-            for (fileName in config.fileNames) {
-                fileListModel!!.addElement(fileName)
-            }
+        slideShowPause.text = config.slideshowPause.toString()
+        fileListModel.clear()
+        for (fileName in config.fileNames) {
+            fileListModel.addElement(fileName)
         }
     }
 
@@ -49,19 +47,19 @@ class BorderConfigPanel : EditorConfigUI() {
      */
     fun save(): BackgroundConfiguration {
         config.name = nameTextField.text
-        config.editorGroup = matchTextField!!.text
-        config.opacity = ((opacitySlider.value / 100.0).toFloat())
-        config.position = (positionComboBox.selectedIndex)
-        config.positionOffset = (Integer.valueOf(positionOffsetTextField!!.text))
-        config.shrink = (shrinkCheckBox.isSelected)
-        config.shrinkValue = (shrinkSlider.value)
-        config.random = (randomCheckBox.isSelected)
-        config.fixedPosition = (fixedPositionCheckBox!!.isSelected)
-        config.shrinkToFit = (shrinkToFitCheckBox!!.isSelected)
-        config.slideshow = (slideshowCheckBox!!.isSelected)
-        config.slideshowPause = (slideShowPause!!.text.toInt())
+        config.editorGroup = matchTextField.text
+        config.opacity = (opacitySlider.value / 100.0).toFloat()
+        config.position = positionComboBox.selectedIndex
+        config.positionOffset = positionOffsetTextField.text.toInt()
+        config.shrink = shrinkCheckBox.isSelected
+        config.shrinkValue = shrinkSlider.value
+        config.random = randomCheckBox.isSelected
+        config.fixedPosition = fixedPositionCheckBox.isSelected
+        config.shrinkToFit = shrinkToFitCheckBox.isSelected
+        config.slideshow = slideshowCheckBox.isSelected
+        config.slideshowPause = slideShowPause.text.toInt()
         val files = arrayOf<String>()
-        fileListModel!!.copyInto(files)
+        fileListModel.copyInto(files)
         config.fileNames = files
         return config
     }
@@ -71,16 +69,16 @@ class BorderConfigPanel : EditorConfigUI() {
      */
     fun isModified(): Boolean {
         return ((nameTextField.text != config.name
-                || matchTextField!!.text != config.editorGroup || opacitySlider.value != (config.opacity * 100).toInt()) || positionComboBox.selectedIndex != config.position || positionOffsetTextField!!.text != java.lang.String.valueOf(
+                || matchTextField.text != config.editorGroup || opacitySlider.value != (config.opacity * 100).toInt()) || positionComboBox.selectedIndex != config.position || positionOffsetTextField.text != java.lang.String.valueOf(
             config.positionOffset
-        ) || shrinkCheckBox.isSelected != config.shrink || shrinkSlider.value != config.shrinkValue || randomCheckBox.isSelected != config.random || fixedPositionCheckBox!!.isSelected != config.fixedPosition || shrinkToFitCheckBox!!.isSelected != config.shrinkToFit || slideshowCheckBox!!.isSelected != config.slideshow || slideShowPause!!.text != java.lang.String.valueOf(
+        ) || shrinkCheckBox.isSelected != config.shrink || shrinkSlider.value != config.shrinkValue || randomCheckBox.isSelected != config.random || fixedPositionCheckBox.isSelected != config.fixedPosition || shrinkToFitCheckBox.isSelected != config.shrinkToFit || slideshowCheckBox.isSelected != config.slideshow || slideShowPause.text != java.lang.String.valueOf(
             config.slideshowPause
         )
                 || !equalFileListModel(config.fileNames))
     }
 
     private fun equalFileListModel(otherList: Array<String>?): Boolean {
-        val size = fileListModel!!.size
+        val size = fileListModel.size
         if (otherList == null) {
             return size == 0
         }
@@ -88,7 +86,7 @@ class BorderConfigPanel : EditorConfigUI() {
             return false
         }
         for (i in 0 until size) {
-            if (fileListModel!![i] != otherList[i]) {
+            if (fileListModel[i] != otherList[i]) {
                 return false
             }
         }
